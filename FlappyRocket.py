72f5ae72e8dbc22c9d0b29436c2538e360e0a4e2
KubaMajchrzak <93284535+KubaMajchrzak@users.noreply.github.com>
2021-10-28 21:13:11 +0200

    FlappyRocket v1.2

--- FlappyRocket.py
+++ FlappyRocket.py
@@ -33,21 +33,28 @@ def move_pipes(pipes):
 def check_collision(pipes):
     for pipe in pipes:
         if player_rect.colliderect(pipe):
-            pygame.quit()
-            sys.exit()
-        if player_rect.top <= 0: 
-            pygame.quit()
-            sys.exit()
+            return False
+    if player_rect.top <= -100 or player_rect.top >= 700:
+        return False
+    return True
+
+
+def rotate_player(player):
+    new_player = pygame.transform.rotozoom(player, 0 - player_movement * 3.5, 1)
+    return new_player
 
 
 pygame.init()
 window = pygame.display.set_mode((400, 600))
+pygame.display.set_caption("Flappy Rocket")
+pygame.display.set_icon(pygame.image.load("_spaceship_.png"))
 clock = pygame.time.Clock()
 
 # game variables
 gravity = 0.25
 player_movement = 0
 speed = 3
+game_active = True
 
 player_surface = pygame.image.load("_spaceship_.png")
 player_surface = pygame.transform.scale(player_surface, (50, 50))
@@ -74,29 +81,38 @@ while True:
             if event.key == pygame.K_ESCAPE:
                 pygame.quit()
                 sys.exit()
-            if event.key == pygame.K_SPACE:
+            if event.key == pygame.K_SPACE and game_active:
                 player_movement = -7.5
+            if event.key == pygame.K_SPACE and game_active == False:
+                pipe_list.clear()
+                player_rect.center = (50, 300)
+                player_movement = -5
+                speed = 3
+                game_active = True
+
         if event.type == SPAWNPIPE:
             pipe_list.extend(create_pipe())
 
     window.fill((81, 120, 181))
     bg_x -= speed / 4
     draw_bg()
-    speed += 0.0000001
+    speed += 0.0001
 
-    # player
-    player_movement += gravity
-    player_rect.centery += player_movement
-    window.blit(player_surface, player_rect)
-    check_collision(pipe_list)
+    if game_active:
+        # player
+        player_movement += gravity
+        rotated_player = rotate_player(player_surface)
+        player_rect.centery += player_movement
+        window.blit(rotated_player, player_rect)
+        game_active = check_collision(pipe_list)
+
+        # pipes
+        pipe_list = move_pipes(pipe_list)
+        draw_pipes(pipe_list)
 
     # background movement
     if bg_x <= -400:
         bg_x = 0
-
-    # pipes
-    pipe_list = move_pipes(pipe_list)
-    draw_pipes(pipe_list)
 
     pygame.display.update()
     clock.tick(60)
